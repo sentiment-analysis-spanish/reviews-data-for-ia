c2b6576f092f06301bd83aabc0fc05f39367d22b
hugo <hjbello.wk@gmail.com>
2020-04-13 13:13:24 +0200

    new data

---
+++ extract-reviews-to-directory/compress_bundle_json_file.sh
@@ -0,0 +1,4 @@
+cd ..
+cd data/json_bundle_reviews
+#gunzip -ck large-bundle-corona.json > reviews-180k-bundle-after-corona.json.gz
+zip reviews-600k-bundle-20200413-full-bundle.zip large-bundle.json 
